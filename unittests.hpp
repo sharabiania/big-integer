170d166939b15002594e3083a5d415b9c2098dd0
sharabiania <sharabiani.a@gmail.com>
2017-08-13 19:26:08 -0400

    Modified > to compare in unittests
    the main function to be tested is the compare() here, all other comarisons will use this function. Plus the assert function already uses != operator, so the unit test was not 100% accurate.

--- unittests.hpp
+++ unittests.hpp
@@ -22,32 +22,33 @@ void assert(type expected, type actual, string message = "") {
 void compareTest() {
     cout << "# compareTest" << endl;
    
-    /// operator> tests
+    /// greater than tests
    
     // both negative: same length
-    assert(false, (bigint)"-12" > (bigint)"-11", "both negative, same lenghts, lhs smaller value");
-    assert(true, bigint("-11") > bigint("-12"), "both negative, same length, lhs bigger value");
+    assert(-1, bigint("-12").compare((bigint)"-11"), "both negative, same lenghts, lhs smaller value");
+    assert(1, bigint("-11").compare(bigint("-12")), "both negative, same length, lhs bigger value");
     
     // both negative different length
-    assert(true, bigint("-9999") > bigint("-99999"), "both negative, different lengths, lhs shorter lenght");
-    assert(false, bigint("-988") > bigint("98"), "both negative, different lengths, lhs longer.");
+    assert(1, bigint("-9999").compare(bigint("-99999")), "both negative, different lengths, lhs shorter lenght");
+    assert(-1, bigint("-988").compare(bigint("98")), "both negative, different lengths, lhs longer.");
     
     // one negative.
-    assert(true, bigint("123") > bigint("-23"), "one negative with smaller lengths");
-    assert(false, bigint("-129") > bigint("192"), "one negatvie same lenghts");
-    assert(true, bigint("29") > bigint("-123"), "one negative with longer lenght");
+    assert(1, bigint("123").compare(bigint("-23")), "one negative with smaller lengths");
+    assert(-1, bigint("-129").compare(bigint("192")), "one negatvie same lenghts");
+    assert(1, bigint("29").compare(bigint("-123")), "one negative with longer lenght");
     
     // both positive
-    assert(true, bigint("19") > bigint("2"), "both positive, lhs longer lenght");
-    assert(true, bigint("199") > bigint("179"), "both positive same lenghts, lhs bigger value");
-    assert(false, bigint("900") > bigint("991"), "both positive, same length, lhs smaller value");
-    assert(false, bigint("2") > bigint("19"), "both positive, lhs smaller lenght");
+    assert(1, bigint("19").compare(bigint("2")), "both positive, lhs longer lenght");
+    assert(1, bigint("199").compare(bigint("179")), "both positive same lenghts, lhs bigger value");
+    assert(-1, bigint("900").compare(bigint("991")), "both positive, same length, lhs smaller value");
+    assert(-1, bigint("2").compare(bigint("19")), "both positive, lhs smaller lenght");
     
     // compare with zero.
-    assert(true, bigint("0") > bigint("-1"), "zero and negative");
-    assert(true, bigint("1") > bigint("0"), "zero and positive");
-    assert(false, bigint("00") > bigint("0"), "both zeros, different lengths");
-    assert(false, bigint("000") > bigint("000"), "both zeros, same lenghts");
+    assert(1, bigint("0").compare(bigint("-1")), "zero and negative rhs");
+    assert(-1, bigint("-1").compare(bigint("0")), "zero and negative lfs");
+    assert(1, bigint("1").compare(bigint("0")), "zero and positive");
+    assert(0, bigint("00").compare(bigint("0")), "both zeros, different lengths");
+    assert(0, bigint("000").compare(bigint("000")), "both zeros, same lenghts");
     
     
 }
