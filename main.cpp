abcd0bc9c335e22d0b6513da918578dfb07d9e2b
sharabiania <sharabiani.a@gmail.com>
2017-08-13 17:23:10 -0400

    Cleaned up the main().

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 
 int main() {
 
-   
-   //if(bigint ("-1") > (bigint)"-10") cout << "passed";
-   compareTest();
-   //increamentTest();
+    compareTest();
+    increamentTest();
    
 	cin.get();
     return 0;
